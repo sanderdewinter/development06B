1cf6e6a5785ab0e98d086f2fd2654e504932db26
Sander de Winter <sanderdewinter@gmail.com>
2014-12-09 13:37:09 +0100

    Update b.sh

--- b-opdracht/b.sh
+++ b-opdracht/b.sh
@@ -4,7 +4,7 @@ then
 	exit 1
 fi
 
-wget -q -O - http://pkg.jenkins-ci.org/debian/jenkins-ci.org.key | sudo apt-key add -
-echo "deb http://pkg.jenkins-ci.org/debian binary/" >> /etc/aptsources.list
-apt-get update
-apt-get install jenkins
+wget -q -O - https://jenkins-ci.org/debian/jenkins-ci.org.key | sudo apt-key add -
+sudo sh -c 'echo deb http://pkg.jenkins-ci.org/debian binary/ > /etc/apt/sources.list.d/jenkins.list'
+sudo apt-get update
+sudo apt-get install jenkins
